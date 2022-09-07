faab0f225bc2c0657a05d630099ccc6d947e3ffa
HyeonsooKim <gustn9442@naver.com>
2022-09-07 19:07:37 +0900

    Feat: DRF Swagger 추가

--- apps/urls.py
+++ apps/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
     path('boards/', include('apps.boards.urls')),
+    path('schema', SpectacularAPIView.as_view(), name='schema'),
+    path('swagger', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('redoc', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
--- config/settings.py
+++ config/settings.py
@@ -53,6 +53,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
+    'drf_spectacular',
+    'drf_spectacular_sidecar',
     'apps.boards.apps.BoardsConfig'
 ]
 
@@ -121,6 +123,15 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.AllowAny",
     ),
     "TEST_REQUEST_DEFAULT_FORMAT": "json",
+}
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Bulletin Board Service',
+    'DESCRIPTION': 'Provide Bulletin Board Service with password authentification and weather api',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    'SWAGGER_UI_DIST': 'SIDECAR',  # shorthand to use the sidecar instead
+    'SWAGGER_UI_FAVICON_HREF': 'SIDECAR',
+    'REDOC_DIST': 'SIDECAR',
 }
 
 LANGUAGE_CODE = 'en-us'
